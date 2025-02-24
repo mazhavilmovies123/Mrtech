a007657dc4487737f066a9e96a48a1ac2dbaa387
𝗗𝗿 𝗕𝗲𝗻𝗷𝗮𝗺𝗶𝗻 𝗟𝗼𝘂𝗶𝘀 <157006183+rolexpc@users.noreply.github.com>
2025-02-25 01:04:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -15,14 +15,10 @@ BATCH_FILES = {}
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        buttons = [
-            [
-                InlineKeyboardButton('🤖 Uᴘᴅᴀᴛᴇs', url=f"https://t.me/filmfactorygroup1")
-            ],
-            [
-                InlineKeyboardButton('ℹ️ Hᴇʟᴘ', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            ]
-            ]
+        buttons = [[            
+            InlineKeyboardButton('🛠️ Help 🛠️', callback_data='help'),
+            InlineKeyboardButton('🤾‍♂ Source Code', url=f"https://t.me/Dr_Benjamin_Louis_01")
+        ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         #await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
@@ -35,11 +31,9 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')          
-            ],[
+        buttons = [[            
             InlineKeyboardButton('🛠️ Help 🛠️', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('🤾‍♂ Source Code', url=f"https://t.me/Dr_Benjamin_Louis_01")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_video(
@@ -78,11 +72,9 @@ async def start(client, message):
             )
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
-        buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[            
+        buttons = [[            
             InlineKeyboardButton('🛠️ Help 🛠️', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('🤾‍♂ Source Code', url=f"https://t.me/Dr_Benjamin_Louis_01")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_video(
