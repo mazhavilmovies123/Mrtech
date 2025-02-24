f62d97f401cd470ad9483abe0b915086d6fdf755
𝗗𝗿 𝗕𝗲𝗻𝗷𝗮𝗺𝗶𝗻 𝗟𝗼𝘂𝗶𝘀 <157006183+rolexpc@users.noreply.github.com>
2025-02-24 19:58:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -36,10 +36,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/filmfactorygroup1')
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')          
             ],[
             InlineKeyboardButton('🛠️ Help 🛠️', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
@@ -83,10 +80,7 @@ async def start(client, message):
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/filmfactorygroup1')
-            ],[
+            ],[            
             InlineKeyboardButton('🛠️ Help 🛠️', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
         ]]
