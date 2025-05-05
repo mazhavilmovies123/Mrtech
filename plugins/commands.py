1678e8b230e2dd5cc9bd1f5b7de747edf227e4d4
CyberGramMovies <arunathira7902@gmail.com>
2025-05-06 02:22:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -17,7 +17,7 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[            
             InlineKeyboardButton('🆘 Help ', callback_data='help'),
-            InlineKeyboardButton('🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ', url=f"https://t.me/Leomc_bot")
+            InlineKeyboardButton('🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ', url=f"https://t.me/Cyber_Fcracker")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
@@ -32,7 +32,7 @@ async def start(client, message):
     if len(message.command) != 2:
         buttons = [[            
             InlineKeyboardButton('🆘 Help ', callback_data='help'),
-            InlineKeyboardButton('🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ', url=f"https://t.me/Leomc_bot")
+            InlineKeyboardButton('🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ', url=f"https://t.me/Cyber_Fcracker")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(
@@ -72,7 +72,7 @@ async def start(client, message):
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[            
             InlineKeyboardButton('🆘 Help ', callback_data='help'),
-            InlineKeyboardButton('🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ', url=f"https://t.me/Leomc_bot")
+            InlineKeyboardButton('🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ', url=f"https://t.me/Cyber_Fcracker")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(
